5dca25ec77932150a8a60a710cf8ec5906e80f60
LeandroFerraz31 <leandro.santos@universo.univates.br>
2024-08-03 22:13:39 -0300

    feat: inclundo Biome para ajustar eslint e substituir o pritter e componentes

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'devburger',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+    
+}
