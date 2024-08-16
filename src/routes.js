637bc8943eed5bdb115a400d309ddc841acf6c1f
LeandroFerraz31 <leandro.santos@universo.univates.br>
2024-08-16 15:34:49 -0300

    feat: ajusta rotas para integrar middleware de autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import authMiddleware from './middlewares/authmid';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -13,6 +14,8 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.post('/products', upload.single('file'), productController.store);
 
