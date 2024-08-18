92a90b1eeff6bc9ebb408d8aa547a59e244cf28e
LeandroFerraz31 <leandro.santos@universo.univates.br>
2024-08-18 18:52:45 -0300

    feat: adicionar validação de produto e associação com categoria

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,14 +1,13 @@
-
 import * as Yup from 'yup';
 import Product from '../models/Product';
-
+import Category from '../models/Category';
 
 class ProductController {
 	async store(request, response) {
 		const schema = Yup.object({
 			name: Yup.string().required(),
 			price: Yup.number().required(),
-			category: Yup.string().required(),
+			category_id: Yup.number().required(),
 		});
 
 		try {
@@ -19,32 +18,30 @@ class ProductController {
 
 		const { filename: path } = request.file;
 
-		const {name, price, category } = request.body
+		const { name, price, category_id } = request.body;
 
-		const {id} = await Product.create({
-			
+		const product = await Product.create({
 			name,
-            price,
-            category,
-            path,
-        });
+			price,
+			category_id,
+			path,
+		});
 
-		
-		
-		
-
-		return response.status(201).json({id,
-			name,
-            price,
-            category,
-            path,});
+		return response.status(201).json(product);
 	}
 	async index(request, response) {
-        const products = await Product.findAll();
+		const products = await Product.findAll({
+			include: [
+				{
+					model: Category,
+					as: 'category',
+					attributes: ['id', 'name'],
+				},
+			],
+		});
 
-        return response.json(products);
-    }
-
+		return response.json(products);
+	}
 }
 
 export default new ProductController();
